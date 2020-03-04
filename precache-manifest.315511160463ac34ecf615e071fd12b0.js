e5f2e2e38d323ab7f65e4171c94bc85bf1d81bec
Agnieszka <agusia2721@gmail.com>
2020-03-04 18:11:47 +0100

    Updates

---
+++ precache-manifest.315511160463ac34ecf615e071fd12b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be12f2f8d5af9e9eefc84af6295af29",
+    "url": "/app-react-router/index.html"
+  },
+  {
+    "revision": "4de77680cc0332aafcc1",
+    "url": "/app-react-router/static/css/main.ac23964e.chunk.css"
+  },
+  {
+    "revision": "6ae5f6bbcd3889eb64f5",
+    "url": "/app-react-router/static/js/2.36a92537.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/app-react-router/static/js/2.36a92537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de77680cc0332aafcc1",
+    "url": "/app-react-router/static/js/main.321318ec.chunk.js"
+  },
+  {
+    "revision": "ea795b22ce77ed3d59cb",
+    "url": "/app-react-router/static/js/runtime-main.ab8e1c94.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-react-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-react-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-react-router/static/media/header3.d9d36b65.jpg"
+  }
+]);
